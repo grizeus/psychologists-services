7db29c54154af95cd13da75ff9d568d61fff8215
grizeus <zaytsewww@gmail.com>
2025-04-05 22:26:43 +0300

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import useStore from "../zustand/store";
 import AuthBar from "./AuthBar";
 import NavBar from "./NavBar";
-import UserBar from "./UserBAr";
+import UserBar from "./UserBar";
 
 const Header = () => {
   const user = useStore(state => state.user);
   return (
-    <header className="w-full border-b border-waterloo/10">
+    <header className="border-waterloo/10 w-full border-b">
       <div className="mx-auto flex max-w-360 items-center justify-between px-32 py-6 sm:min-w-80">
         <p className="text-xl leading-6 font-bold tracking-tight">
           <span className="text-sun">psychologists</span>
